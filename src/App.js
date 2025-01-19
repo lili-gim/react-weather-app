ec6f35392f7d56d0f968f93fc979c5dad0aed2f3
lili-gim <156603091+lili-gim@users.noreply.github.com>
2025-01-19 16:53:14 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Lili Gim and is{" "}
+        <a href="https://github.com/lili-gim/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
